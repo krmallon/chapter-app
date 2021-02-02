a8607dc1ef196bcc4427ecf4d13b2847dcb03048
Kevin Mallon <kevinmallon.uk@gmail.com>
2021-02-02 14:36:35 +0000

    Create initial Flask back-end (#2)

--- back-end/app.py
+++ back-end/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+
 import configparser
 
 app = Flask(__name__)
+CORS(app)
 
 config = configparser.ConfigParser()
 config.read("config.py")
@@ -10,3 +13,6 @@ DB_URI = config.get("DEFAULT", "DB_URI")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
