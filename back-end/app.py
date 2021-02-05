747fd6b450b8dd5d90fc46c9187013bbebff586c
Kevin Mallon <kevinmallon.uk@gmail.com>
2021-02-05 21:52:27 +0000

    Add endpoints for following/unfollowing users and adding new user to DB (#3)

--- back-end/app.py
+++ back-end/app.py
@@ -14,6 +14,9 @@ DB_URI = config.get("DEFAULT", "DB_URI")
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 db = SQLAlchemy(app)
 
+def user_in_db(user):
+    return db2.session.query(User.user_id).filter_by(auth0_id=user).scalar() is not None
+
 @app.route("/api/v1.0/auth0/<auth0_id>", methods=["GET"])
 def get_user_id_by_auth0(auth0_id):
     try:
@@ -28,15 +31,15 @@ def get_user_id(auth0_id):
 
 @app.route("/api/v1.0/user/<user_id>", methods=["GET"])
 def get_user_details(user_id):
-    data = []
+    data_to_return = []
 
     try:
-        result = db2.session.query(User).filter_by(user_id=user_id).first()
+        result = db.session.query(User).filter_by(user_id=user_id).first()
     except:
         return make_response(jsonify({"error" : "Invalid ID"}), 404)
     
     user = {"user_id" : result.user_id, "auth0_id" : result.auth0_id, "name" : result.full_name}
-    data.append(user)
+    data_to_return.append(user)
 
     return make_response(jsonify(data), 200)
 
@@ -48,19 +51,54 @@ def search_users(query):
     req = requests.get(url, headers=headers)
     js = req.json()
 
-    data = []
+    data_to_return = []
 
     for result in js:
         try:
             user = {"auth0_id" : result['user_id'], "name" : result['name'], "email" : result['email']}
-            data.append(user)
+            data_to_return.append(user)
         except Exception:
             pass
 
-    if data:
-        return make_response(jsonify(data), 200)
+    if data_to_return:
+        return make_response(jsonify(data_to_return), 200)
     else:
         return make_response(jsonify({"error" : "No results for this query"}), 404)
+
+@app.route("/api/v1.0/user/<string:user_id>/followedby/<string:follower_id>", methods=["GET"])
+def check_following(user_id, follower_id):
+    exists = db2.session.query(Follow.id).filter_by(user_id=user_id, follower_id=follower_id).scalar() is not None
+    
+    if exists:
+        return make_response(jsonify(exists), 200)
+    else:
+        return make_response(jsonify(exists), 404)
+
+@app.route("/api/v1.0/user/<string:user_id>/followers", methods=["GET"])
+def get_followers(user_id):
+    results = db.session.query(Follow.follower_id, Follow.follow_date).filter_by(user_id=user_id)
+
+    data_to_return = []
+
+    for follower in results:
+        user = {"user_id" : follower.follower_id, "follow_date" : follower.follow_date}
+        data_to_return.append(user)
+
+    if data_to_return:
+        return make_response(jsonify(data_to_return), 200)
+    else:
+        return make_response(jsonify({"error" : "This user has 0 followers or does not exist"}), 404)
+
+@app.route("/api/v1.0/userprofiletodb/<auth0_id>/<name>/<email>", methods=["POST"])
+def send_profile_to_db(auth0_id, name, email):
+    
+    if not user_in_db(auth0_id):
+        db2.session.add(User(auth0_id = auth0_id, full_name=name, nickname='km', email=email))
+        db2.session.commit()
+        
+        return make_response(jsonify("User added to DB"), 200)
+    else: 
+        return make_response(jsonify({"error" : "User already exists in DB"}), 400)
 
 if __name__ == "__main__":
     app.run(debug=True)
