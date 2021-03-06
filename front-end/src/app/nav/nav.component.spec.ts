1297f730fb64d69789451c4839dc361194a5d553
Kevin Mallon <kevinmallon.uk@gmail.com>
2021-03-06 19:15:51 +0000

    Create initial Nav component (#16)

---
+++ front-end/src/app/nav/nav.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavComponent } from './nav.component';
+
+describe('NavComponent', () => {
+  let component: NavComponent;
+  let fixture: ComponentFixture<NavComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have correct app brand name label', () => {
+    let brand = "Chapter"
+    expect(component.brand).toEqual(brand)
+  })
+});
