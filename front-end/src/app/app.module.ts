eff42fc80312469181e8765edfb16e841656d3f8
Kevin Mallon <kevinmallon.uk@gmail.com>
2021-03-07 20:14:39 +0000

    Update app.module to include new components and their paths

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,26 +6,54 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { BookComponent } from './book/book.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { UserService } from './services/user.service';
+import { AuthService } from './auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 var routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'user/:id',
+    component: ProfileComponent
+  },
+  {
+    path: 'books/:id',
+    component: BookComponent
+  },
+  {
+    path: '404',
+    component: NotfoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/404'
   }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    BookComponent,
+    ProfileComponent,
+    NotfoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(routes),
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
