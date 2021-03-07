d2e3fddd1e3bb2ec9c6f7ff71f18a667d7fe61d7
Kevin Mallon <kevinmallon.uk@gmail.com>
2021-03-07 20:13:05 +0000

    Create initial userService class (#20)

---
+++ front-end/src/app/services/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export class UserService {
+
+    currentUser;
+
+    constructor(private http: HttpClient) {} 
+
+    getCurrentUser(auth0_id) {
+        return this.http.get('http://localhost:5000/api/v1.0/auth0/' + auth0_id).subscribe(
+            response => {
+                sessionStorage.setItem("user_id", this.currentUser)
+            }
+        )
+    }
+}
