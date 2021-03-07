63ad2b783fe3a6fd513d6d7856543f253c884f89
Kevin Mallon <kevinmallon.uk@gmail.com>
2021-03-07 20:09:28 +0000

    Add footer and update navbar elements (#16)

--- front-end/src/app/nav/nav.component.ts
+++ front-end/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,7 @@ export class NavComponent implements OnInit {
 
   brand = "Chapter"
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
   }
