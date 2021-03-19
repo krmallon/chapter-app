ec3ba5af76a31b049fa3380c56d3af87302b4060
Kevin Mallon <kevinmallon.uk@gmail.com>
2021-03-19 18:39:14 +0000

    Implement searchBooks feature (#27)

--- front-end/src/app/services/search.service.ts
+++ front-end/src/app/services/search.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  private book_results_private_list;
+  private bookResultsSubject = new Subject();
+  book_results_list = this.bookResultsSubject.asObservable();
+
+  private user_results_private_list;
+  private userResultsSubject = new Subject();
+  user_results_list = this.userResultsSubject.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  searchBooks(query) {
+    return this.http.get('http://localhost:5000/api/v1.0/search/' + query).subscribe(
+      response => {
+        this.book_results_private_list = response;
+        this.bookResultsSubject.next(this.book_results_private_list);
+      })
+  }
+
+  searchUsers(query) {
+
+  }
+
+
 }
