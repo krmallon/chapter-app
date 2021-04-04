f4da64692b926c06f456e88621b2a0ca76aa10e4
Kevin Mallon <kevinmallon.uk@gmail.com>
2021-04-04 18:38:37 +0100

    Add basic profile page showing currently reading and achievements (#19)

--- front-end/src/app/profile/profile.component.ts
+++ front-end/src/app/profile/profile.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { AchievementService } from '../services/achievement.service';
+import { BookService } from '../services/book.service';
 import { GoalService } from '../services/goal.service';
+import { ReviewService } from '../services/review.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,13 +14,35 @@ import { GoalService } from '../services/goal.service';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(public goalService: GoalService, private route: ActivatedRoute) { }
+  constructor(public authService: AuthService, public goalService: GoalService, public userService: UserService, public bookService: BookService, public reviewService: ReviewService, public achievementService: AchievementService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.userService.getProfileDetails(this.route.snapshot.params.id)
+    this.reviewService.getReviewsByUser(this.route.snapshot.params.id)
     this.goalService.getGoal(this.route.snapshot.params.id)
+    this.userService.checkFollowing(this.route.snapshot.params.id, sessionStorage.user_id)
+    this.bookService.getCurrentlyReadingByUser(this.route.snapshot.params.id)
+    this.achievementService.getUserAchievements(this.route.snapshot.params.id)
   }
 
   getGoalCompletion(current, target) {
     return (current / target) * 100
   }
+
+  isOwnProfile() {
+    return this.route.snapshot.params.id == sessionStorage.user_id;
+  }
+
+  following() {
+    return this.userService.following
+  }
+
+  follow() {
+    this.userService.followUser(this.route.snapshot.params.id, sessionStorage.user_id)
+  }
+
+  unfollow() {
+    this.userService.unfollowUser(this.route.snapshot.params.id, sessionStorage.user_id)
+  }
+
 }
