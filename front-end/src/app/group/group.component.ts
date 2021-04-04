fa5d00a6e2b0aca42deda9fa23f66b8aaf18cdc4
Kevin Mallon <kevinmallon.uk@gmail.com>
2021-04-04 18:37:15 +0100

    Update group layout and functionality (#35)

--- front-end/src/app/group/group.component.ts
+++ front-end/src/app/group/group.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommentService } from '../services/comment.service';
 import { GroupService } from '../services/group.service';
 
 @Component({
@@ -9,12 +10,18 @@ import { GroupService } from '../services/group.service';
 })
 export class GroupComponent implements OnInit {
 
-  constructor(public groupService: GroupService, private route: ActivatedRoute) { }
+  POST_OBJECT_TYPE = 6;
+
+  constructor(public groupService: GroupService, public commentService: CommentService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.groupService.getGroup(this.route.snapshot.params.id)
     this.groupService.getMembers(this.route.snapshot.params.id)
     this.groupService.getPosts(this.route.snapshot.params.id)
+  }
+
+  getPostComments(post_id) {
+    this.commentService.getComments(this.POST_OBJECT_TYPE, post_id)
   }
 
 }
