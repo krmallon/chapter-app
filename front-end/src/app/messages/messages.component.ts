7bcf742e2b47fb153632b198ce59a963122e4505
Kevin Mallon <kevinmallon.uk@gmail.com>
2021-04-04 18:36:20 +0100

    Create initial Messages implementation (#26) (#28)

--- front-end/src/app/messages/messages.component.ts
+++ front-end/src/app/messages/messages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { MessagingService } from '../services/messaging.service';
 
 @Component({
   selector: 'app-messages',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  newMessageForm;
+  partner;
+
+  constructor(public messagingService: MessagingService, private formBuilder : FormBuilder, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.newMessageForm = this.formBuilder.group({
+      message: ['', Validators.required]
+    });
+
+    this.messagingService.getReceivedMessages(sessionStorage.user_id)
+    this.messagingService.getChatPartners(sessionStorage.user_id)
+    if (this.chatOpen()) {
+      this.messagingService.getConversation(sessionStorage.user_id, this.route.snapshot.params.user_id)
+    }
+  }
+  
+  setPartner(partner) {
+    this.partner = partner.id;
+    console.log(this.partner)
+  }
+  
+  loadChat(partner_id) {
+    console.log("loading chat with " + partner_id)
+    this.messagingService.getConversation(sessionStorage.user_id, partner_id)
+  }
+  
+  sentMessage(msg) {
+    return msg.sender == sessionStorage.user_id
+  }
+  
+  receivedMessage(msg) {
+    return msg.recipient == sessionStorage.user_id
+  }
+  
+  onSend() {
+    console.log(this.newMessageForm.value)
+    this.messagingService.sendMessage(sessionStorage.user_id, this.route.snapshot.params.user_id, this.newMessageForm.value)
+    this.newMessageForm.reset()
+    this.loadChat(this.route.snapshot.params.user_id)
+    // this.webService.getChatPartners(sessionStorage.user_id)
+    // console.log(this.route.snapshot.params.user_id)
+  }
+  
+  chatOpen() {
+    return this.route.snapshot.params.user_id
   }
 
 }
--- front-end/src/app/services/messaging.service.ts
+++ front-end/src/app/services/messaging.service.ts
@@ -1,9 +1,72 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagingService {
 
-  constructor() { }
+  private rec_messages_private_list;
+  private recMessagesSubject = new Subject();
+  rec_messages_list = this.recMessagesSubject.asObservable();
+
+  private convo_messages_private_list;
+  private convoMessagesSubject = new Subject();
+  convo_messages_list = this.convoMessagesSubject.asObservable();
+
+  private msg_partners_private_list;
+  private msgPartnersSubject = new Subject()
+  msg_partners_list = this.msgPartnersSubject.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  getReceivedMessages(user_id) {
+    return this.http.get('http://localhost:5000/api/v1.0/user/' + user_id + '/messages/received').subscribe(
+        response => {
+            this.rec_messages_private_list = response;
+            this.recMessagesSubject.next(this.rec_messages_private_list);
+        }
+    )
+}
+
+getConversation(user_id, partner_id) {
+    return this.http.get('http://localhost:5000/api/v1.0/messages/?userA=' + user_id + '&userB=' + partner_id).subscribe(
+        response => {
+            this.convo_messages_private_list = response;
+            this.convoMessagesSubject.next(this.convo_messages_private_list);
+        }
+    )
+}
+
+getChatPartners(user_id) {
+  return this.http.get('http://localhost:5000/api/v1.0/user/' + user_id + '/messages/participants').subscribe(
+      response => {
+          this.msg_partners_private_list = response;
+          this.msgPartnersSubject.next(this.msg_partners_private_list);
+      }
+  )
+  
+}
+
+sendMessage(sender_id, recipient_id, message) {
+  let postData = new FormData();
+
+  // append user ID and user nickname from Auth0 profile object
+  // postData.append("id", "10");
+  postData.append("sender_id", sender_id);
+  postData.append("recipient_id", recipient_id);
+  postData.append("msg_text", message.message);
+  // postData.append("rating", review.rating);
+
+  this.http.post(
+      'http://localhost:5000/api/v1.0/user/' + recipient_id + '/contact',
+      postData).subscribe(
+      response => {
+      // this.getBookID(isbn)
+      // this.getReviews(isbn);
+      } );
+}
+
+
 }
